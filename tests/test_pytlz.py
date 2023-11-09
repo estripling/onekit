6ce27acee82a7e385988c32fc3598ce0c1c9cdf5
estripling <estripling042@gmail.com>
2023-11-09 17:01:57 +0100

    feat(pytlz): add num_to_str

---
+++ tests/test_pytlz.py
@@ -0,0 +1,19 @@
+import pytest
+
+import onekit
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (1, "1"),
+        (10, "10"),
+        (100, "100"),
+        (1000, "1_000"),
+        (1000000, "1_000_000"),
+        (100000.0, "100_000.0"),
+    ],
+)
+def test_num_to_str(n, expected):
+    actual = onekit.num_to_str(n)
+    assert actual == expected
