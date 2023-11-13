895590e1aad950ddffbeff3d5c0fddb4316119cd
estripling <estripling042@gmail.com>
2023-11-13 14:51:38 +0000

    feat(pytlz): add flatten

--- src/onekit/pytlz.py
+++ src/onekit/pytlz.py
@@ -1,10 +1,17 @@
 """Python utility functions."""
 
 import datetime as dt
-from typing import Union
+from typing import (
+    Generator,
+    Iterator,
+    List,
+    Sequence,
+    Union,
+)
 
 __all__ = (
     "date_to_str",
+    "flatten",
     "num_to_str",
 )
 
@@ -20,6 +27,32 @@ def date_to_str(d: dt.date, /) -> str:
     '2022-01-01'
     """
     return d.isoformat()
+
+
+def flatten(*items: List) -> Generator:
+    """Flatten collection of items.
+
+    Examples
+    --------
+    >>> from onekit import pytlz
+    >>> list(pytlz.flatten([[1, 2], *[3, 4], [5]]))
+    [1, 2, 3, 4, 5]
+
+    >>> list(pytlz.flatten([1, (2, 3)], 4, [], [[[5]], 6]))
+    [1, 2, 3, 4, 5, 6]
+
+    >>> list(pytlz.flatten(["one", 2], 3, [(4, "five")], [[["six"]]], "seven", []))
+    ['one', 2, 3, 4, 'five', 'six', 'seven']
+    """
+
+    def _flatten(items):
+        for item in items:
+            if isinstance(item, (Iterator, Sequence)) and not isinstance(item, str):
+                yield from _flatten(item)
+            else:
+                yield item
+
+    return _flatten(items)
 
 
 def num_to_str(n: Union[int, float], /) -> str:
