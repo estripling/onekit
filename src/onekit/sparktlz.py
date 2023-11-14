24db0ca56195317177fd7e4b9384be75432ad2c6
estripling <estripling042@gmail.com>
2023-11-14 09:35:29 +0000

    test: ignore Spark doctest

--- src/onekit/sparktlz.py
+++ src/onekit/sparktlz.py
@@ -15,13 +15,13 @@ def union(*dataframes: Sequence[SparkDF]) -> SparkDF:
 
     Examples
     --------
-    >>> from pyspark.sql import SparkSession  # doctest: +SKIP
-    >>> from onekit import sparktlz  # doctest: +SKIP
-    >>> spark = SparkSession.builder.getOrCreate()  # doctest: +SKIP
-    >>> df1 = spark.createDataFrame([dict(x=1, y=2), dict(x=3, y=4)])  # doctest: +SKIP
-    >>> df2 = spark.createDataFrame([dict(x=5, y=6), dict(x=7, y=8)])  # doctest: +SKIP
-    >>> df3 = spark.createDataFrame([dict(x=0, y=1), dict(x=2, y=3)])  # doctest: +SKIP
-    >>> sparktlz.union(df1, df2, df3).show()  # doctest: +SKIP
+    >>> from pyspark.sql import SparkSession
+    >>> from onekit import sparktlz
+    >>> spark = SparkSession.builder.getOrCreate()
+    >>> df1 = spark.createDataFrame([dict(x=1, y=2), dict(x=3, y=4)])
+    >>> df2 = spark.createDataFrame([dict(x=5, y=6), dict(x=7, y=8)])
+    >>> df3 = spark.createDataFrame([dict(x=0, y=1), dict(x=2, y=3)])
+    >>> sparktlz.union(df1, df2, df3).show()
     +---+---+
     |  x|  y|
     +---+---+
