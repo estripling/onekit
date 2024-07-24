405aa2d913b3627b001b7cb80d2fc4964b0625e9
estripling <estripling042@gmail.com>
2024-07-24 21:49:56 +0200

    feat: add precision given recall (#83)
    * feat(sklearnkit): add precision_recall_values
    
    * feat(sklearnkit): add precision_given_recall
    
    * feat(sklearnkit): add precision_given_recall_score
    
    * feat(sklearnkit): add precision_given_recall_summary

--- src/onekit/sklearnkit.py
+++ src/onekit/sklearnkit.py
@@ -8,15 +8,50 @@ import numpy.typing as npt
 import pandas as pd
 from pandas import DataFrame as PandasDF
 from sklearn import metrics
-from sklearn.utils import validation
 
 __all__ = (
+    "precision_given_recall",
     "precision_given_recall_score",
-    "threshold_summary",
+    "precision_given_recall_summary",
+    "precision_recall_values",
 )
 
 
 ArrayLike = npt.ArrayLike
+
+
+def precision_given_recall(
+    y_true: ArrayLike,
+    y_score: ArrayLike,
+    *,
+    min_recall: float,
+    pos_label: Optional[Union[int, str]] = None,
+) -> PandasDF:
+    """Compute precision given a desired recall level.
+
+    Examples
+    --------
+    >>> import onekit.sklearnkit as slk
+    >>> import pandas as pd
+    >>> y_true = [0, 1, 1, 1, 0, 0, 0, 1]
+    >>> y_score = [0.1, 0.4, 0.35, 0.8, 0.5, 0.2, 0.75, 0.5]
+    >>> with pd.option_context("display.float_format", "{:.2f}".format):
+    ...     slk.precision_given_recall(y_true, y_score, min_recall=0.7)
+       threshold  precision  recall
+    0       0.40       0.60    0.75
+    """
+    if not (0 < min_recall <= 1):
+        raise ValueError(f"{min_recall=} - must be a float in (0, 1]")
+    return (
+        precision_recall_values(y_true, y_score, pos_label=pos_label)
+        .query(f"recall >= {min_recall}")
+        .assign(min_empirical_recall=lambda df: df["recall"].min())
+        .query("recall == min_empirical_recall")
+        .drop(columns=["min_empirical_recall"])
+        .sort_values("precision", ascending=False)
+        .reset_index(drop=True)
+        .head(1)
+    )
 
 
 def precision_given_recall_score(
@@ -26,7 +61,7 @@ def precision_given_recall_score(
     min_recall: float,
     pos_label: Optional[Union[int, str]] = None,
 ) -> float:
-    """Compute precision given a desired minimum recall level.
+    """Compute precision score given a desired recall level.
 
     Examples
     --------
@@ -36,118 +71,113 @@ def precision_given_recall_score(
     >>> slk.precision_given_recall_score(y_true, y_score, min_recall=0.7)
     0.6
     """
-    if not (0 < min_recall <= 1):
-        raise ValueError(f"{min_recall=} - must be a float in (0, 1]")
+    return precision_given_recall(
+        y_true,
+        y_score,
+        min_recall=min_recall,
+        pos_label=pos_label,
+    ).at[0, "precision"]
 
-    df = (
-        threshold_summary(y_true, y_score, pos_label=pos_label)
-        .filter(items=["precision", "recall"])
-        .query(f"recall >= {min_recall}")
+
+def precision_given_recall_summary(
+    y_true: ArrayLike,
+    y_score: ArrayLike,
+    *,
+    min_recall: float,
+    pos_label: Optional[Union[int, str]] = None,
+) -> PandasDF:
+    """Extend `precision_given_recall` with additional performance metrics.
+
+    Examples
+    --------
+    >>> import onekit.sklearnkit as slk
+    >>> import pandas as pd
+    >>> y_true = [0, 1, 1, 1, 0, 0, 0, 1]
+    >>> y_score = [0.1, 0.4, 0.35, 0.8, 0.5, 0.2, 0.75, 0.5]
+    >>> with pd.option_context("display.float_format", "{:.4f}".format):
+    ...     slk.precision_given_recall_summary(y_true, y_score, min_recall=0.7).T
+                            0
+    threshold          0.4000
+    predicted_positive 5.0000
+    true_positive      3.0000
+    false_positive     2.0000
+    false_negative     1.0000
+    true_negative      2.0000
+    precision          0.6000
+    recall             0.7500
+    f1                 0.6667
+    accuracy           0.6250
+    balanced_accuracy  0.6250
+    matthews_corrcoef  0.2582
+    pos_lr             1.5000
+    neg_lr             0.5000
+    """
+    pr = precision_given_recall(
+        y_true,
+        y_score,
+        min_recall=min_recall,
+        pos_label=pos_label,
+    )
+    t = pr.at[0, "threshold"]
+    y_pred = y_score >= t
+    tn, fp, fn, tp = metrics.confusion_matrix(y_true, y_pred).ravel()
+
+    pos_label = 1 if pos_label is None else pos_label
+    neg_label = set(y_true).difference({pos_label}).pop()
+    labels = [neg_label, pos_label]
+    pos_lr, neg_lr = metrics.class_likelihood_ratios(y_true, y_pred, labels=labels)
+
+    data = dict(
+        threshold=t,
+        predicted_positive=tp + fp,
+        true_positive=tp,
+        false_positive=fp,
+        false_negative=fn,
+        true_negative=tn,
+        precision=pr.at[0, "precision"],
+        recall=pr.at[0, "recall"],
+        f1=metrics.f1_score(y_true, y_pred),
+        accuracy=metrics.accuracy_score(y_true, y_pred),
+        balanced_accuracy=metrics.balanced_accuracy_score(y_true, y_pred),
+        matthews_corrcoef=metrics.matthews_corrcoef(y_true, y_pred),
+        pos_lr=pos_lr,
+        neg_lr=neg_lr,
     )
 
-    min_empirical_recall = df["recall"].min()
-
-    return float(
-        0
-        if df.empty
-        else df.query(f"recall == {min_empirical_recall}")["precision"].max()
-    )
+    return pd.DataFrame([data])
 
 
-def threshold_summary(
+def precision_recall_values(
     y_true: ArrayLike,
     y_score: ArrayLike,
     *,
     pos_label: Optional[Union[int, str]] = None,
 ) -> PandasDF:
-    """Threshold summary.
+    """Compute precision-recall pairs for all thresholds.
 
     Notes
     -----
-    - Support for binary classification only
-    - Assumpution: classifier returns scores
-    - First values correspond to the edge case where everything is predicted positive
-    - Last values correspond to the edge case where everything is predicted negative
+    Output of `sklearn.metrics.precision_recall_curve` is wrapped in a DataFrame.
 
     Examples
     --------
     >>> import onekit.sklearnkit as slk
+    >>> import pandas as pd
     >>> y_true = [0, 1, 1, 1, 0, 0, 0, 1]
     >>> y_score = [0.1, 0.4, 0.35, 0.8, 0.5, 0.2, 0.75, 0.5]
     >>> with pd.option_context("display.float_format", "{:.2f}".format):
-    ...     slk.threshold_summary(y_true, y_score).T
-                          0    1    2    3    4    5    6    7
-    threshold          0.10 0.20 0.35 0.40 0.50 0.75 0.80  inf
-    predicted_positive 8.00 7.00 6.00 5.00 4.00 2.00 1.00 0.00
-    true_positive      4.00 4.00 4.00 3.00 2.00 1.00 1.00 0.00
-    false_positive     4.00 3.00 2.00 2.00 2.00 1.00 0.00 0.00
-    false_negative     0.00 0.00 0.00 1.00 2.00 3.00 3.00 4.00
-    true_negative      0.00 1.00 2.00 2.00 2.00 3.00 4.00 4.00
-    precision          0.50 0.57 0.67 0.60 0.50 0.50 1.00 1.00
-    recall             1.00 1.00 1.00 0.75 0.50 0.25 0.25 0.00
-    f1                 0.67 0.73 0.80 0.67 0.50 0.33 0.40 0.00
-    accuracy           0.50 0.62 0.75 0.62 0.50 0.50 0.62 0.50
-    balanced_accuracy  0.50 0.62 0.75 0.62 0.50 0.50 0.62 0.50
-    matthews_corrcoef   NaN 0.38 0.58 0.26 0.00 0.00 0.38  NaN
+    ...     slk.precision_recall_values(y_true, y_score)
+       threshold  precision  recall
+    0       0.10       0.50    1.00
+    1       0.20       0.57    1.00
+    2       0.35       0.67    1.00
+    3       0.40       0.60    0.75
+    4       0.50       0.50    0.50
+    5       0.75       0.50    0.25
+    6       0.80       1.00    0.25
+    7        inf       1.00    0.00
     """
-    y = validation.column_or_1d(y_true)
-    s = validation.column_or_1d(y_score)
-    validation.check_consistent_length(y, s)
-    validation.assert_all_finite(y)
-    validation.assert_all_finite(s)
-    pos_label = validation._check_pos_label_consistency(pos_label, y)
-
-    precision, recall, thresholds = metrics.precision_recall_curve(
-        y_true=y,
-        y_score=s,
-        pos_label=pos_label,
-        sample_weight=None,
-        drop_intermediate=False,
-    )
-
-    is_true_pos = y == pos_label
-    is_true_neg = y != pos_label
-
-    def is_pred_pos(t: float) -> np.ndarray:
-        return s >= t
-
-    def is_pred_neg(t: float) -> np.ndarray:
-        return s < t
-
-    return (
-        pd.DataFrame(np.append(thresholds, np.inf), columns=["t"])
-        .assign(
-            pp=lambda df: df.t.map(lambda t: is_pred_pos(t).sum()),
-            tp=lambda df: df.t.map(lambda t: (is_pred_pos(t) & is_true_pos).sum()),
-            fp=lambda df: df.t.map(lambda t: (is_pred_pos(t) & is_true_neg).sum()),
-            fn=lambda df: df.t.map(lambda t: (is_pred_neg(t) & is_true_pos).sum()),
-            tn=lambda df: df.t.map(lambda t: (is_pred_neg(t) & is_true_neg).sum()),
-            precision=precision,
-            recall=recall,
-            f1=2 * (precision * recall) / (precision + recall),
-            acc=lambda df: (df.tp + df.tn) / (df.tp + df.tn + df.fp + df.fn),
-            bacc=lambda df: 0.5 * (df.tp / (df.tp + df.fn) + df.tn / (df.tn + df.fp)),
-            mcc=lambda df: np.true_divide(
-                (df.tp * df.tn - df.fp * df.fn),
-                np.sqrt(
-                    (df.tp + df.fp)
-                    * (df.tp + df.fn)
-                    * (df.tn + df.fp)
-                    * (df.tn + df.fn)
-                ),
-            ),
-        )
-        .rename(
-            columns={
-                "t": "threshold",
-                "pp": "predicted_positive",
-                "tp": "true_positive",
-                "fp": "false_positive",
-                "fn": "false_negative",
-                "tn": "true_negative",
-                "acc": "accuracy",
-                "bacc": "balanced_accuracy",
-                "mcc": "matthews_corrcoef",
-            },
-        )
+    p, r, t = metrics.precision_recall_curve(y_true, y_score, pos_label=pos_label)
+    return pd.DataFrame(
+        {"threshold": np.append(t, np.inf), "precision": p, "recall": r}
     )
--- tests/test_sklearnkit.py
+++ tests/test_sklearnkit.py
@@ -1,75 +1,75 @@
 import numpy as np
 import pandas as pd
 import pytest
-from pandas import DataFrame as PandasDF
-from sklearn import metrics
 
 import onekit.sklearnkit as slk
 
 
-@pytest.mark.parametrize(
-    "min_recall, expected",
-    [
-        (0, None),
-        (0.7, 0.6),
-        (0.75, 0.6),
-        (0.80, 2 / 3),
-        (1.0, 2 / 3),
-    ],
-)
-def test_precision_given_recall_score(min_recall: float, expected: float):
-    y_true = np.array([0, 1, 1, 1, 0, 0, 0, 1])
-    y_score = np.array([0.1, 0.4, 0.35, 0.8, 0.5, 0.2, 0.75, 0.5])
+class TestPrecisionRecall:
+    def test_precision_given_recall(self, y_true: np.ndarray, y_score: np.ndarray):
+        actual = slk.precision_given_recall(y_true, y_score, min_recall=0.7)
+        expected = pd.DataFrame([dict(threshold=0.4, precision=0.6, recall=0.75)])
+        pd.testing.assert_frame_equal(actual, expected)
 
-    if min_recall > 0:
-        actual = slk.precision_given_recall_score(
-            y_true, y_score, min_recall=min_recall
-        )
-        assert actual == expected
-    else:
         with pytest.raises(ValueError):
-            slk.precision_given_recall_score(y_true, y_score, min_recall=min_recall)
+            slk.precision_given_recall(y_true, y_score, min_recall=0)
 
+    def test_precision_given_recall_score(
+        self,
+        y_true: np.ndarray,
+        y_score: np.ndarray,
+    ):
+        actual = slk.precision_given_recall_score(y_true, y_score, min_recall=0.7)
+        expected = 0.6
 
-def test_threshold_summary(expected_threshold_summary: PandasDF):
-    y_true = np.array([0, 1, 1, 1, 0, 0, 0, 1])
-    y_score = np.array([0.1, 0.4, 0.35, 0.8, 0.5, 0.2, 0.75, 0.5])
+        assert isinstance(actual, float)
+        assert actual == expected
 
-    actual = slk.threshold_summary(y_true, y_score)
-    pd.testing.assert_frame_equal(actual, expected_threshold_summary)
-
-    for t in expected_threshold_summary["threshold"]:
-        y_pred = y_score >= t
-
-        actual_scores = (
-            actual.query(f"threshold == {t}")
-            .filter(
-                [
-                    "precision",
-                    "recall",
-                    "f1",
-                    "accuracy",
-                    "balanced_accuracy",
-                    "matthews_corrcoef",
-                ],
-            )
-            .fillna(0)
-            .reset_index(drop=True)
-            .T
+    def test_precision_given_recall_summary(
+        self,
+        y_true: np.ndarray,
+        y_score: np.ndarray,
+    ):
+        actual = slk.precision_given_recall_summary(y_true, y_score, min_recall=0.7)
+        expected_data = dict(
+            threshold=0.4,
+            predicted_positive=5,
+            true_positive=3,
+            false_positive=2,
+            false_negative=1,
+            true_negative=2,
+            precision=0.6,
+            recall=0.75,
+            f1=2 / 3,
+            accuracy=0.625,
+            balanced_accuracy=0.625,
+            matthews_corrcoef=0.2581988897471611,
+            pos_lr=1.5,
+            neg_lr=0.5,
         )
+        expected = pd.DataFrame([expected_data])
+        pd.testing.assert_frame_equal(actual, expected)
 
-        expected_scores = pd.Series(
-            {
-                # precision_recall_curve assigns 1 instead of 0 for precision
-                "precision": (
-                    1.0 if np.isinf(t) else metrics.precision_score(y_true, y_pred)
-                ),
-                "recall": metrics.recall_score(y_true, y_pred),
-                "f1": metrics.f1_score(y_true, y_pred),
-                "accuracy": metrics.accuracy_score(y_true, y_pred),
-                "balanced_accuracy": metrics.balanced_accuracy_score(y_true, y_pred),
-                "matthews_corrcoef": metrics.matthews_corrcoef(y_true, y_pred),
-            }
-        ).to_frame()
+    def test_precision_recall_values(self, y_true: np.ndarray, y_score: np.ndarray):
+        actual = slk.precision_recall_values(y_true, y_score)
+        expected = pd.DataFrame(
+            [
+                dict(threshold=0.1, precision=0.5, recall=1.0),
+                dict(threshold=0.2, precision=4 / 7, recall=1.0),
+                dict(threshold=0.35, precision=2 / 3, recall=1.0),
+                dict(threshold=0.4, precision=0.6, recall=0.75),
+                dict(threshold=0.5, precision=0.5, recall=0.5),
+                dict(threshold=0.75, precision=0.5, recall=0.25),
+                dict(threshold=0.8, precision=1.0, recall=0.25),
+                dict(threshold=np.inf, precision=1.0, recall=0.0),
+            ]
+        )
+        pd.testing.assert_frame_equal(actual, expected)
 
-        pd.testing.assert_frame_equal(actual_scores, expected_scores)
+    @pytest.fixture()
+    def y_true(self) -> np.ndarray:
+        return np.array([0, 1, 1, 1, 0, 0, 0, 1])
+
+    @pytest.fixture()
+    def y_score(self) -> np.ndarray:
+        return np.array([0.1, 0.4, 0.35, 0.8, 0.5, 0.2, 0.75, 0.5])
