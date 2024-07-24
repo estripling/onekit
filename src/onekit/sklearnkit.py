9cc488b59d08cf4274c2d2a6dc8fd676f8129321
estripling <estripling042@gmail.com>
2024-07-24 21:00:56 +0200

    feat(sklearnkit): add precision_given_recall

--- src/onekit/sklearnkit.py
+++ src/onekit/sklearnkit.py
@@ -9,10 +9,47 @@ import pandas as pd
 from pandas import DataFrame as PandasDF
 from sklearn import metrics
 
-__all__ = ("precision_recall_values",)
+__all__ = (
+    "precision_given_recall",
+    "precision_recall_values",
+)
 
 
 ArrayLike = npt.ArrayLike
+
+
+def precision_given_recall(
+    y_true: ArrayLike,
+    y_score: ArrayLike,
+    *,
+    min_recall: float,
+    pos_label: Optional[Union[int, str]] = None,
+) -> PandasDF:
+    """Compute precision given a desired recall level.
+
+    Examples
+    --------
+    >>> import onekit.sklearnkit as slk
+    >>> import pandas as pd
+    >>> y_true = [0, 1, 1, 1, 0, 0, 0, 1]
+    >>> y_score = [0.1, 0.4, 0.35, 0.8, 0.5, 0.2, 0.75, 0.5]
+    >>> with pd.option_context("display.float_format", "{:.2f}".format):
+    ...     slk.precision_given_recall(y_true, y_score, min_recall=0.7)
+       threshold  precision  recall
+    0       0.40       0.60    0.75
+    """
+    if not (0 < min_recall <= 1):
+        raise ValueError(f"{min_recall=} - must be a float in (0, 1]")
+    return (
+        precision_recall_values(y_true, y_score, pos_label=pos_label)
+        .query(f"recall >= {min_recall}")
+        .assign(min_empirical_recall=lambda df: df["recall"].min())
+        .query("recall == min_empirical_recall")
+        .drop(columns=["min_empirical_recall"])
+        .sort_values("precision", ascending=False)
+        .reset_index(drop=True)
+        .head(1)
+    )
 
 
 def precision_recall_values(
--- tests/test_sklearnkit.py
+++ tests/test_sklearnkit.py
@@ -6,6 +6,14 @@ import onekit.sklearnkit as slk
 
 
 class TestPrecisionRecall:
+    def test_precision_given_recall(self, y_true: np.ndarray, y_score: np.ndarray):
+        actual = slk.precision_given_recall(y_true, y_score, min_recall=0.7)
+        expected = pd.DataFrame([dict(threshold=0.4, precision=0.6, recall=0.75)])
+        pd.testing.assert_frame_equal(actual, expected)
+
+        with pytest.raises(ValueError):
+            slk.precision_given_recall(y_true, y_score, min_recall=0)
+
     def test_precision_recall_values(self, y_true: np.ndarray, y_score: np.ndarray):
         actual = slk.precision_recall_values(y_true, y_score)
         expected = pd.DataFrame(
