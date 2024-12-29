6d5fba112bd00ec7d7584ba776dcc5db6cee1151
estripling <estripling042@gmail.com>
2024-12-29 12:50:24 +0100

    test(Population): add list methods tests

--- tests/test_dekit.py
+++ tests/test_dekit.py
@@ -64,6 +64,53 @@ class TestPopulation:
         pop = dek.Population(dek.Individual(0), dek.Individual(1))
         assert pop.size == 2
 
+    def test_list_methods(self):
+        ind1 = dek.Individual([0, 0])
+        ind2 = dek.Individual([1, 1])
+        ind3 = dek.Individual([2, 2])
+
+        pop = dek.Population(ind1, ind2, ind3)
+        assert pop.size == 3
+        assert pop.index(ind1) == 0
+        assert pop.count(ind1) == 1
+        assert pop[0] == ind1
+
+        ind = pop.pop(0)
+        assert pop.size == 2
+        assert ind == ind1
+        assert pop == [ind2, ind3]
+
+        pop.append(ind)
+        assert pop.size == 3
+        assert pop == [ind2, ind3, ind1]
+
+        pop.remove(ind1)
+        assert pop.size == 2
+        assert pop == [ind2, ind3]
+
+        pop.insert(0, ind1)
+        assert pop.size == 3
+        assert pop == [ind1, ind2, ind3]
+
+        pop.reverse()
+        assert pop.size == 3
+        assert pop == [ind3, ind2, ind1]
+        pop.reverse()
+        assert pop == [ind1, ind2, ind3]
+
+        ind4 = dek.Individual([3, 3])
+        ind5 = dek.Individual([4, 4])
+        pop.extend([ind4, ind5])
+        assert pop.size == 5
+        assert pop == [ind1, ind2, ind3, ind4, ind5]
+
+        pop.clear()
+        assert pop.size == 0
+
+        pop.extend([ind1, ind1])
+        assert pop.size == 2
+        assert pop == [ind1, ind1]
+
 
 @pytest.mark.parametrize("ind", [dek.Individual([0, 0]), None, 1, "two"])
 def test_check_individual_type(ind: dek.Individual):
